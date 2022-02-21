d9bad89c8d78197526dada813e71bdf24bc0b187
Ethan Wall <53667981+EthanWall@users.noreply.github.com>
2022-02-20 22:15:24 -0500

    Removed accidentally pushed util method

--- util.py
+++ util.py
@@ -1,6 +1,3 @@
-import typing
-
-
 class Units:
 
     @staticmethod
@@ -10,12 +7,3 @@ class Units:
     @staticmethod
     def inches_to_metres(inches: float):
         return inches * 0.0254
-
-
-class Scheduler:
-    tasks: list[tuple[typing.Callable, float]]
-
-    def schedule_task(self, task: typing.Callable, time: float):
-        self.tasks.append((task, time))
-
-
